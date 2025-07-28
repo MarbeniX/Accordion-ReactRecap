a4e1d486d2e2853b3a6ef5d4e1f3770e99a15783
Berlin Ortega <marlitros200002@gmail.com>
2025-07-28 15:04:29 -0600

    dinamy children prop

--- src/components/Acoordion.jsx
+++ src/components/Acoordion.jsx
@@ -1,15 +1,21 @@
 import AccordionItem from "./AccordionItem";
+import { useState } from "react";
 
 export default function Acoordion({ data }) {
+    const [curOpen, setCurOpen] = useState(null);
+
     return (
         <div className="accordion">
             {data.map((item, index) => (
                 <AccordionItem
+                    curOpen={curOpen}
+                    onOpen={setCurOpen}
                     title={item.title}
-                    text={item.text}
                     num={index}
                     key={item.title + index}
-                />
+                >
+                    {item.text}
+                </AccordionItem>
             ))}
         </div>
     );
